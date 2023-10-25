a9ea7c926fd66c37c53e1cc69b6be56e0bf58229
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-25 14:54:08 +0530

    Update person.js

--- person.js
+++ person.js
@@ -1,1 +1,28 @@
+class Person {
+   constructor(firstName, lastName, age, gender) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+      this.gender = gender;
+   }
+   getFullName() {
+     return `${this.firstName} ${this.lastName}`;
+   }
+   getAge() {
+     return this.age;
+   }
+   updateAge(newAge) {
+      this.age = newAge;
+   }
+   greet() {
+     const salutation = this.gender = 'male' ? 'Mr.' : 'Ms.';
+     return `Hello, ${salutation} ${this.lastName}!`;
+   }
+}
+const person1 = new Person("chaitrra", "Sairam", 32, "female");
+console.log(person1.getFullName());
+console.log(person1.getAge());
+console.log(person1.greet());
 
+person1.updateAge(34);
+console.log(person1.getAge());
